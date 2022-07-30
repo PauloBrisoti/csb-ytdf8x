b3cdd60e68e2ae2989696d6edb889a6a3b8f07ea
Code Sandbox <hello@codesandbox.io>
2022-07-30 01:27:11 +0000

    Deployment from Codesandbox.

---
+++ js/index.js
@@ -0,0 +1,37 @@
+//Função do botão revelar resposta
+function revelarResposta() {
+  var resposta = document.querySelector("#resposta");
+  resposta.classList.toggle("blur");
+}
+
+function proximaPergunta(proximaPergunta) {
+  var pergunta = document.querySelector("#cardContainer");
+  pergunta.innerHTML = "";
+
+  var cardDiv = document.createElement("div");
+  cardDiv.classList.add("card", "animate__animated", "animate__backInRinght");
+
+  cardDiv.innerHTML = `
+  <div class="card-cabecalho centralizar">  
+    <h1 class="card-titulo">O que é ${proximaPergunta.title}?</h1>
+  </div>
+
+  <div id="resposta" class="card-conteudo blur"> 
+    <p>${proximaPergunta.description}</p>
+  </div>
+`;
+  // link: flash.quickstaart.com/random
+  pergunta.appendChild(cardDiv);
+}
+
+function buscarInformacao() {
+  fetch("https://flash.quickstaart.com/random")
+    .then(function (resultado) {
+      return resultado.json();
+    })
+    .then(function (resultadoJson) {
+      proximaPergunta(resultadoJson);
+    });
+}
+
+window.addEventListener("load", buscarInformacao);
